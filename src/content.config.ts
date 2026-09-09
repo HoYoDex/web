2119c2a70c1a7ebe22b49bf76a29919e57f21658
AlzyWelzy <tech@hoyodex.com>
2026-09-09 12:05:40 +0530

    Update dependencies and improve import structure in content configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 import { mediaWikiLoader } from './loaders/mediawiki';
 import { GAMES } from './lib/games';
 
